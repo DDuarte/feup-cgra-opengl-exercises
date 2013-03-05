c4b5314478c7bf0ca358b529be4e9a09f8bcc3c1
ei11086 <ei11086@1715d095-ed64-4316-968e-d33a6178d55d>
2013-03-05 16:54:28 +0000

    git-svn-id: https://svn.fe.up.pt/repos/cgra1213_15@10 1715d095-ed64-4316-968e-d33a6178d55d

--- Exercise1/src/TPscene.cpp
+++ Exercise1/src/TPscene.cpp
@@ -11,42 +11,42 @@ float deg2rad=pi/180.0;
 
 CGFappearance *mat1;
 
-void TPscene::init()
+void TPscene::init() 
 {
-    // Enables lighting computations
-    glEnable(GL_LIGHTING);
+	// Enables lighting computations
+	glEnable(GL_LIGHTING);
 
-    // Sets up some lighting parameters
-    glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, CGFlight::background_ambient);  // Define ambient light
+	// Sets up some lighting parameters
+	glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, CGFlight::background_ambient);  // Define ambient light
+	
+	// Declares and enables a light
+	float light0_pos[4] = {4.0, 6.0, 5.0, 1.0};
+	CGFlight* light0 = new CGFlight(GL_LIGHT0, light0_pos);
+	light0->enable();
 
-    // Declares and enables a light
-    float light0_pos[4] = {4.0, 6.0, 5.0, 1.0};
-    CGFlight* light0 = new CGFlight(GL_LIGHT0, light0_pos);
-    light0->enable();
-
-    // Defines a default normal
-    glNormal3f(0,0,1);
+	// Defines a default normal
+	glNormal3f(0,0,1);
 
 }
 
-void TPscene::display()
+void TPscene::display() 
 {
-    // Clear image and depth buffer everytime we update the scene
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	// Clear image and depth buffer everytime we update the scene
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-    // Initialize Model-View matrix as identity (no transformation)
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+	// Initialize Model-View matrix as identity (no transformation)
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
-    // Apply transformations corresponding to the camera position relative to the origin
-    CGFscene::activeCamera->applyView();
+	// Apply transformations corresponding to the camera position relative to the origin
+	CGFscene::activeCamera->applyView();
 
-    // Draw axis
-    axis.draw();
+	// Draw axis
+	axis.draw();
 
     glPushMatrix();
-    chair.draw();
+	chair.draw();
     /*glTranslatef(4.0f, 0.00f, 3.0f);
 
     floor.draw();
@@ -56,10 +56,10 @@ void TPscene::display()
     table.draw();
     glPopMatrix();
 
-    */
+	*/
 
     glPopMatrix();
 
     // swap buffers
-    glutSwapBuffers();
+	glutSwapBuffers();
 }
