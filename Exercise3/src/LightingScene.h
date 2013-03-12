2cb76758614f4643bc72a98c0c196942038624f1
ei11101 <ei11101@1715d095-ed64-4316-968e-d33a6178d55d>
2013-03-12 16:48:25 +0000

    Exercise3 initial code
    git-svn-id: https://svn.fe.up.pt/repos/cgra1213_15@13 1715d095-ed64-4316-968e-d33a6178d55d

---
+++ Exercise3/src/LightingScene.h
@@ -0,0 +1,35 @@
+#ifndef LightingScene_H
+#define LightingScene_H
+
+#include "CGFscene.h"
+#include "CGFappearance.h"
+#include "myTable.h"
+#include "myChair.h"
+#include "Plane.h"
+
+class LightingScene : public CGFscene
+{
+public:
+	void init();
+	void display();
+
+	CGFlight* light0;
+	CGFlight* light1;
+    CGFlight* light2;
+    CGFlight* light3;
+
+	myTable* table;
+    myChair* chair;
+	Plane* wall;
+	Plane* boardA;
+	Plane* boardB;
+	CGFappearance* materialA;
+	CGFappearance* materialB;
+    CGFappearance* materialW;
+    CGFappearance* materialF;
+    CGFappearance* materialC;
+
+	~LightingScene();
+};
+
+#endif
