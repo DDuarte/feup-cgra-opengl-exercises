44b2b97e06d3e783aa3b176c2001cd82b32438d3
ei11086 <ei11086@1715d095-ed64-4316-968e-d33a6178d55d>
2013-03-12 18:48:38 +0000

    Implement myCylinder
    git-svn-id: https://svn.fe.up.pt/repos/cgra1213_15@15 1715d095-ed64-4316-968e-d33a6178d55d

--- Exercise3/src/myCylinder.cpp
+++ Exercise3/src/myCylinder.cpp
@@ -1,5 +1,63 @@
 #include "myCylinder.h"
 
 myCylinder::myCylinder(int slices, int stacks/*, bool smooth*/) {
-	
+	_slices = slices;
+	_stacks = stacks;
+
+	const double angle = 2*PI / _slices;
+
+	double ang = 0.0;
+	double z = 0.0;
+	for(int j = 0; j < _stacks ; ++j)
+	{
+		std::vector<Point> aux;
+		vec.push_back(aux);
+
+		for(int i = 0 ; i < _slices ; ++i)
+		{
+			Point p(cos(ang),sin(ang),z);
+			vec[j].push_back(p);
+		
+				ang += angle;
+		}
+
+		z += 1.0/_stacks;
+	}
+
+}
+
+void myCylinder::draw()
+{
+		glBegin(GL_POLYGON);
+		for(int i = _slices - 1 ; i >= 0 ; --i)
+				glVertex3d(vec[0][i].x , vec[0][i].y , vec[0][i].z);
+		glEnd();
+
+		glBegin(GL_POLYGON);
+		for(int i = 0 ; i < _slices ; ++i)
+			glVertex3d(vec[_stacks-1][i].x , vec[_stacks-1][i].y , vec[_stacks-1][i].z);
+		glEnd();
+
+		for(int i = 0 ; i < _stacks - 1; ++i)
+			for(int j = 0 ; j < _slices; ++j)
+			{
+				if(j == _slices - 1)
+				{
+					glBegin(GL_POLYGON);
+					glVertex3d(vec[i][j].x,vec[i][j].y,vec[i][j].z);
+					glVertex3d(vec[i][0].x,vec[i][0].y,vec[i][0].z);
+					glVertex3d(vec[i+1][0].x,vec[i+1][0].y,vec[i+1][0].z);
+					glVertex3d(vec[i+1][j].x,vec[i+1][j].y,vec[i+1][j].z);
+					glEnd();
+				}
+				else{
+
+					glBegin(GL_POLYGON);
+					glVertex3d(vec[i][j].x,vec[i][j].y,vec[i][j].z);
+					glVertex3d(vec[i][j+1].x,vec[i][j+1].y,vec[i][j+1].z);
+					glVertex3d(vec[i+1][j+1].x,vec[i+1][j+1].y,vec[i+1][j+1].z);
+					glVertex3d(vec[i+1][j].x,vec[i+1][j].y,vec[i+1][j].z);
+					glEnd();
+				}
+			}
 }
--- Exercise3/src/myCylinder.h
+++ Exercise3/src/myCylinder.h
@@ -2,12 +2,31 @@
 #define MY_CYLINDER
 
 #include "CGF/CGFobject.h"
+#include <vector>
+#include <cmath>
+
+#define PI       3.14159265358979323846
+
+struct Point
+{
+	double x;
+	double y;
+	double z;
+
+	Point(double x, double y, double z): x(x), y(y), z(z){}
+};
+
+
 
 class myCylinder : public CGFobject {
+private:
+	int _slices;
+	int _stacks;
+	std::vector<std::vector<Point> > vec;
 
-	public:
-		myCylinder(int slices, int stacks/*, bool smooth*/);
-
+public:
+	myCylinder(int slices, int stacks/*, bool smooth*/);
+	void draw();
 };
 
 
